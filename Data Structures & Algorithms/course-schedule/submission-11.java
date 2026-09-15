97dfeb3f90b5a73f538f8ad0dc48cdb02828725d
Arnav-173 <inboxarnav@gmail.com>
2026-09-15 08:47:39 +0530

    Add: course-schedule - submission-11

---
+++ Data Structures & Algorithms/course-schedule/submission-11.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> preMap = new HashMap<>();
+        for (int i = 0; i < prerequisites.length; i++) {
+            List<Integer> adjList = preMap.get(prerequisites[i][0]);
+            if (adjList == null) {
+                adjList = new ArrayList<>();
+                preMap.put(prerequisites[i][0], adjList);
+            }
+            adjList.add(prerequisites[i][1]);
+        }
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(visited, preMap, i)) return false;
+            visited.remove(i);
+        }
+        return true;
+    }
+
+    private boolean dfs(Set<Integer> visited, Map<Integer, List<Integer>> preMap, Integer course) {
+        if (visited.contains(course)) {
+            return false;
+        }
+        visited.add(course);
+        if (preMap.get(course) != null) {
+            for (Integer preCourse : preMap.get(course)) {
+                if (!(dfs(visited, preMap, preCourse)))
+                    return false;
+                visited.remove(preCourse);
+            }
+            preMap.get(course).clear();
+        }
+        return true;
+    }
+}
